e78506cc4a48a6d4205f37e3159f89721777a45b
Rafael Fernandes <rafatexfr@gmail.com>
2019-10-22 06:29:49 +0200

    experimenting with coveralls

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  coverageDirectory: "coverage",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{vue}",
+    "!**/node_modules/**",
+    "!<rootDir>/dist/**",
+    "!<rootDir>/src/plugins/**",
+    "!<rootDir>/tests/unit/**"
+  ],
+  coverageReporters: ["lcov", "text-summary"]
 };
