91f73e3197404dbbdcc74db1b6c0f3aa5d35037a
Rafael Fernandes <rafatexfr@gmail.com>
2019-10-27 19:34:25 +0100

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <!-- Start of toolbar -->
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetiful Weather</span>
+        <span class="font-weight-light">Powered by Vue/tify</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text to="/">Home</v-btn>
+        <v-btn text to="/about">About</v-btn>
+      </v-toolbar-items>
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-app-bar-nav-icon>
+    </v-app-bar>
+    <!-- End of toolbar -->
+
+    <!-- Start of mobile side menu -->
+    <v-navigation-drawer app v-model="drawer" right>
+      <v-list nav>
+        <v-list-item-group>
+          <v-list-item to="/">Home</v-list-item>
+          <v-list-item to="/about">About</v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- End of mobile side menu -->
+
+    <!-- Start of actual app content -->
+    <v-content>
+      <router-view />
+    </v-content>
+    <!-- End of actual app content -->
+  </v-app>
 </template>
+]
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from "vue";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default Vue.extend({
+  name: "App",
+  data: () => ({
+    //
+    drawer: false // Hide mobile side menu by default
+  })
+});
+</script>
---
+++ src/store/state/index.ts
@@ -0,0 +1,38 @@
+export default {};
+
+// location {
+//     coordinates: {
+//         latitude
+//         longitude
+//     }
+// }
+// status: "failed"
+// temperature: undefined,
+// city: undefined,
+// country: undefined,
+// humidity: undefined,
+// description: undefined,
+// error: undefined
+
+// export interface ILocationState {
+//   coordinates: {
+//     latitude: Number | undefined;
+//     longitude: Number | undefined;
+//   };
+//   city: String | undefined;
+//   countryCode: String | undefined;
+// }
+
+// export interface ITemperatureUnitState {
+//   temperatureUnit: TemperatureUnitEnum | undefined;
+// }
+
+// export enum TemperatureUnitEnum {
+//   CELSIUS = "Celsius",
+//   KELVIN = "Kelvin",
+//   FAHRENHEIT = "Fahrenheit"
+// }
+
+// export interface IForecastState {
+//   forecast: any;
+// }
